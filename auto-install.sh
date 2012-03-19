a9837b00d6449c5ad1a6320c2496426f633de15a
Jeremy Kendall <jeremy@jeremykendall.net>
2012-03-19 00:16:21 -0500

    Adds install scripts from https://github.com/vgod/vimrc

---
+++ auto-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copied from https://github.com/vgod/vimrc/blob/master/auto-install.sh
+VIMHOME=~/.vim
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+[ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
+[ -e "~/.vim" ] && die "~/.vim already exists."
+[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+
+git clone git@github.com:jeremykendall/vim.git "$VIMHOME"
+cd "$VIMHOME"
+git submodule update --init
+
+./install-vimrc.sh
+
+echo "vimrc is installed."
---
+++ install-vimrc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Copied from https://github.com/vgod/vimrc/blob/master/install-vimrc.sh
+cd ..
+ln -s .vim/vimrc .vimrc
+ln -s .vim/gvimrc .gvimrc
